db08772580c6e6438d2774169bbfbe931b194072
Prabesh Bhetwal <jarvis.pc13@gmail.com>
2023-06-29 15:15:54 +1000

    Build Changess

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+const app = express();
+import cors from "cors";
+const PORT = 8008;
+
+import path from "path";
+const __dirname = path.resolve();
+console.log(__dirname);
+
+// connect mongodb
+import { mongoConnect } from "./src/config/mongoDb.js";
+mongoConnect();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(express.static(__dirname + "/build"));
+
+// API endpoints
+import taskRouter from "./src/router/taskRouter.js";
+
+app.use("/api/v1/task", taskRouter);
+
+app.use("/", (req, res) => {
+  // res.json({ message: "Server running healthy" });
+  res.sendFile(__dirname + "/index.html");
+});
+
+// open port for http request to access the server
+app.listen(PORT, (err) => {
+  err
+    ? console.log(err.message)
+    : console.log(`Server running at http://localhost:${PORT}`);
+});
